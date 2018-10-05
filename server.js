89a3d35f98c1709e757818d1490d2a464a9e7c32
Håkan MacLean <hakan@maclean.se>
2018-10-05 23:32:25 +0200

    Also scrap Canyon.com

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var nodemailer = require('nodemailer');
 var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 const gmailAddress = settings.email
 const gmailPassword = settings.password
-var bikes = []
-var mailContent = null
+var happyRideBikes = []
+var canyonBikes = []
 var interval = 10 * 60 * 1000 //check every 10 mins
 var maxTimeWithoutEmail = 24 * 60 * 60 * 1000; //24h
 var lastSent = Date.now();
@@ -21,6 +21,7 @@ var transporter = nodemailer.createTransport({
 });
 
 function scrapeHappyRide(callback) {
+  console.log('Scraping HappyRide');
   var pageURL = 'https://happyride.se/annonser/?search=spectral&category=1&county=&type=1&category2=&county2=&type2=&price=&year=';
   request(pageURL, function(error, response, html) {
     if (!error) {
@@ -36,10 +37,9 @@ function scrapeHappyRide(callback) {
           var price = $(elem).children().last().text()
           newBikes[i] = `${name} (${price})`
         })
-        if (!arraysEqual(bikes, newBikes)) {
-          console.log("new bikes found: ", newBikes)
-          bikes = newBikes;
-          body = bikes.join('\n') + "\nURL: " + pageURL
+        if (newBikes.length > happyRideBikes.length) {
+          console.log("new happyRideBikes found: ", newBikes)
+          body = newBikes.join('\n') + "\nURL: " + pageURL
           mailContent = {
             from: gmailAddress,
             to: 'hakan@maclean.se',
@@ -48,9 +48,54 @@ function scrapeHappyRide(callback) {
           };
           callback(mailContent)
         }
+        happyRideBikes = newBikes;
       }
     }
   })
+}
+
+function scrapeCanyon(callback) {
+  console.log('Scraping Canyon.com');
+  var url = 'http://www.canyon.com/sv/factory-outlet/category.html';
+  var Horseman = require('node-horseman');
+  var horseman = new Horseman({
+    injectJquery: true, 
+    ignoreSSLErrors: true,
+    webSecurity: false,
+    loadImages: false,
+  });
+
+  horseman
+    .open(url)
+    .click('a:contains("MTB/GRAVITY")')
+    .waitForSelector('.product-box')
+    // .screenshot('test.png')
+    .html('body')
+    .then((html) => {
+      var $ = cheerio.load(html);
+      var products = $('.product-box');
+      var newCanyonBikes = []
+      products.each(function() {
+        var product = $(this).attr('data-product');
+        var title = JSON.parse(product).name;
+        if (title.includes('Spectral')) {
+          newCanyonBikes.push(title);
+        }
+      });
+      if (newCanyonBikes.length > canyonBikes.length) {
+        console.log("new canyon bikes found: ", newCanyonBikes)
+        body = newCanyonBikes.join('\n') + "\nURL: " + url
+        mailContent = {
+          from: gmailAddress,
+          to: 'hakan@maclean.se',
+          subject: 'Nya cyklar på Canyon',
+          text: body 
+        };
+        callback(mailContent)
+      }
+      canyonBikes = newCanyonBikes;
+    })
+    .close()
 }
 
 function arraysEqual(a1,a2) {
@@ -62,15 +107,14 @@ function sendEmail(mailContent) {
     console.log("sending email: ", mailContent)
     lastSent = Date.now();
 
-    // transporter.sendMail(mailContent, function(error, info){
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     console.log('Email sent: ' + info.response);
-    //     mailContent = null
-    //   }
-    // });
-    mailContent = null          
+    transporter.sendMail(mailContent, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        mailContent = null
+      }
+    });
   }
 }
 
@@ -87,7 +131,7 @@ function sendStatusEmailIfNeeed() {
 }
 
 function scrapePages() {
-  var scrapers = [scrapeHappyRide]
+  var scrapers = [scrapeCanyon, scrapeHappyRide]
   scrapers.forEach(function(scrape) {
     scrape(sendEmail);
   });
